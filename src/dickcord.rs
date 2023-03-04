d8a14752d073769ca4e2d67210ed8a614a1d8d93
Enitoni <tech@enitoni.dev>
2023-03-04 01:34:12 +0100

    hhhhh

--- src/dickcord.rs
+++ src/dickcord.rs
@@ -6,18 +6,34 @@ use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use songbird::SerenityInit;
 
-struct Handler;
+struct Handler {
+    user_id: u64,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn ready(&self, context: Context, ready: Ready) {
+        let user = context
+            .cache
+            .user(self.user_id)
+            .expect("User does not exist");
+
+        user.direct_message(&context, |f| f.content("your mother"))
+            .await
+            .unwrap();
     }
 }
 
 pub async fn dickcord() {
     let token =
         env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment youi fhfjck");
+
+    let user_id: u64 = env::var("DISCORD_USER")
+        .expect("Expected a DISCORD_USER id in the environment. Who am i supposed to follow?")
+        .parse()
+        .unwrap();
+
+    let handler = Handler { user_id };
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
@@ -26,7 +42,7 @@ pub async fn dickcord() {
 
     let mut client = Client::builder(&token, intents)
         .register_songbird()
-        .event_handler(Handler)
+        .event_handler(handler)
         .await
         .expect("Err creating client");
 
