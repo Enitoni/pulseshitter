16e305e2c9c1440671d30470093c2ad2d911b206
Enitoni <tech@enitoni.dev>
2023-03-04 02:02:44 +0100

    I live in a state of constant pain

--- src/dickcord.rs
+++ src/dickcord.rs
@@ -3,6 +3,8 @@ use std::env;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::{GuildId, Member};
+use serenity::model::user::User;
 use serenity::prelude::*;
 use songbird::SerenityInit;
 
@@ -15,15 +17,37 @@ impl EventHandler for Handler {
     async fn ready(&self, context: Context, ready: Ready) {
         println!("Finding user {}", self.user_id);
 
-        let user = context
-            .cache
-            .user(self.user_id)
-            .expect("User does not exist");
+        let guilds = context.cache.guilds();
 
-        user.direct_message(&context, |f| f.content("your mother"))
+        println!("Searching in {} guilds", guilds.len());
+
+        let member = find_member_in_guilds(&context, self.user_id, guilds).await;
+
+        member
+            .expect("User not found")
+            .user
+            .direct_message(&context, |f| f.content("your mother"))
             .await
             .unwrap();
     }
+}
+
+async fn find_member_in_guilds(
+    context: &Context,
+    user_id: u64,
+    guilds: Vec<GuildId>,
+) -> Option<Member> {
+    for guild in guilds {
+        let members = guild.members(context, None, None).await.unwrap();
+
+        for member in members {
+            if member.user.id == user_id {
+                return Some(member);
+            }
+        }
+    }
+
+    None
 }
 
 pub async fn dickcord() {
