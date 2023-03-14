3c36bf777a914d8c20f5287da599dbc4b758ed5d
Seb <enitoni@gmail.com>
2023-03-15 00:39:19 +0100

    Try without blocking?

--- src/audio.rs
+++ src/audio.rs
@@ -86,22 +86,13 @@ impl Default for AudioStream {
 
 impl Read for AudioStream {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let mut bytes_read = 0;
+        let mut lock = self.parec.lock().unwrap();
+        let parec = (*lock).as_mut();
 
-        while bytes_read < buf.len() {
-            let mut lock = self.parec.lock().unwrap();
-            let parec = (*lock).as_mut();
-
-            match parec {
-                Some(parec) => {
-                    bytes_read += parec.stdout.read(buf).unwrap_or_default();
-                    dbg!(&bytes_read);
-                }
-                None => continue,
-            };
+        match parec {
+            Some(parec) => parec.stdout.read(buf),
+            None => Ok(0),
         }
-
-        Ok(bytes_read)
     }
 }
 
