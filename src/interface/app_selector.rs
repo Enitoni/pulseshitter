26f128d94e158ceec37cf41b8b4c48e860bc1d2e
Enitoni <tech@enitoni.dev>
2023-04-20 15:49:36 +0200

    more margin

--- src/interface/app_selector.rs
+++ src/interface/app_selector.rs
@@ -60,7 +60,7 @@ impl Widget for &AppSelector {
         // Add margins
         let block_inner = {
             let area = block.inner(area);
-            tui::layout::Rect::new(area.left() + 1, area.top() + 1, area.height, area.width)
+            tui::layout::Rect::new(area.left() + 2, area.top() + 1, area.height, area.width)
         };
 
         block.render(area, buf);
