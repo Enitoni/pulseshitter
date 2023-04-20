89713f99832bd92352c99d2a5fed20a4ba0fd14d
Seb <enitoni@gmail.com>
2023-04-20 16:57:16 +0200

    try fixing layout again

--- src/interface/dashboard.rs
+++ src/interface/dashboard.rs
@@ -39,7 +39,7 @@ impl Widget for &DashboardView {
     fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints([Constraint::Length(area.width - 64), Constraint::Length(64)])
+            .constraints([Constraint::Length(area.width - 65), Constraint::Length(64)])
             .margin(1)
             .split(area);
 
