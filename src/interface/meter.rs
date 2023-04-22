6b9b4bb27bb3c2ea6acb4079888cc8ec7b9e5241
Enitoni <seb@enitoni.dev>
2023-04-22 02:23:19 +0200

    Add color to meters

--- src/interface/meter.rs
+++ src/interface/meter.rs
@@ -38,9 +38,36 @@ impl Meter {
             Style::default(),
         );
 
+        let mut bar_area = Rect::new(area.x, area.y, area.width, 1);
+
         buf.set_style(
-            Rect::new(area.x, area.y, area.width, 1),
-            Style::default().bg(Color::Black),
+            bar_area,
+            Style::default()
+                .fg(Color::Rgb(82, 224, 45))
+                .bg(Color::Rgb(10, 17, 9)),
+        );
+
+        let yellow_size = (area.width as f32 * 0.4) as u16;
+
+        bar_area.width = yellow_size;
+        bar_area.x = area.width - yellow_size;
+        buf.set_style(
+            bar_area,
+            Style::default()
+                .fg(Color::Rgb(255, 240, 85))
+                .bg(Color::Rgb(17, 16, 9)),
+        );
+
+        let red_size = (area.width as f32 * 0.1) as u16;
+
+        bar_area.width = red_size;
+        bar_area.x = (area.width - red_size) + 2;
+
+        buf.set_style(
+            bar_area,
+            Style::default()
+                .bg(Color::Rgb(17, 10, 9))
+                .fg(Color::Rgb(199, 54, 28)),
         );
     }
 }
